b94439e0c2037677bb0b175d5f6b64c7e65cb6a7
Weslley Addson <51407794+wesquel@users.noreply.github.com>
2021-09-24 15:16:44 -0300

    Update Person.java

--- sample/Person.java
+++ sample/Person.java
@@ -2,13 +2,15 @@ package sample;
 
 public class Person {
 
-    private String id,name,code,imageReference;
+    private String id,name,code,imageReference,lista,pod;
 
-    public Person(String id, String name, String code, String imageReference) {
+    public Person(String id, String name, String code, String imageReference, String lista, String pod) {
         this.id = id;
         this.name = name;
         this.code = code;
         this.imageReference = imageReference;
+        this.lista = lista;
+        this.pod = pod;
     }
 
     public String getId() {
@@ -37,6 +39,22 @@ public class Person {
 
     public String getImageReference() {
         return imageReference;
+    }
+
+    public String getLista() {
+        return lista;
+    }
+
+    public void setLista(String lista) {
+        this.lista = lista;
+    }
+
+    public String getPod() {
+        return pod;
+    }
+
+    public void setPod(String pod) {
+        this.pod = pod;
     }
 
     public void setImageReference(String imageReference) {
